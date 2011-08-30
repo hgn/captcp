1762f32d7da4542fe5eeb24c06a43840687fb1d3
Hagen Paul Pfeifer <hagen@jauu.net>
2011-08-30 22:14:15 +0200

    add a link tp tcpdump
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- data/js/functions.js
+++ data/js/functions.js
@@ -1,23 +1,64 @@
-// remap jQuery to $
-(function($){})(window.jQuery);
-
-
-/* trigger when page is ready */
-$(document).ready(function (){
-
-	// your functions go here
-
-});
-
-
-/* optional triggers
-
-$(window).load(function() {
+$(document).ready(function(){
 	
-});
-
-$(window).resize(function() {
+	$("ul.screenshots a").click(function() {
+		
+		$("div#inner").css("background-image", "url(" + $(this).attr("href") + ")");
+		$("div#overlay").css("top", $(window).scrollTop());
+		$("div#overlay").fadeIn(400);
+		return false;
+		
+	});
 	
-});
+	$("div#overlay").click(function() {
+	
+		$(this).fadeOut(200);
+	
+	});
+	
+	$("a").mouseenter(function() {
+	
+		if ($(this).attr("data-twitter")) {
+		
+			if ($(".twitterInfo").length == 0) {
+			
+				thisPosition = $(this).position();
+				thisWidth = $(this).width();
+				
+				$(this).after("<span class='twitterInfo' style='left: " + (thisPosition.left + thisWidth + 3) + "px; top: " + (thisPosition.top - 8) + "px;'><a href='http://twitter.com/" + $(this).attr("data-twitter") + "'>@" + $(this).attr("data-twitter") + "</a></span>");
+				
+				$("span.twitterInfo").fadeIn(300);
+			
+			};
+		
+		}
+	
+	});
+	
+	$("a").mouseout(function() {
+	
+		if ($(this).attr("data-twitter")) {
+		
+			$("span.twitterInfo").delay(100).fadeOut(300, function () {
+			
+	            $(this).remove();
+			
+			});
+		
+		}
+	
+	});
+	
+	$("span.twitterInfo").live('moseover', function() {
 
-*/
+		$("span.twitterInfo").stop(true,false);
+		$(this).prev().mouseenter();
+	
+	});
+	
+	$("span.twitterInfo").live('mouseleave', function() {
+
+		$(this).prev().mouseout();
+	
+	});
+		
+});
