3cda02fc33d97fc1cf5f714efdcc5d0a15dd1c02
Hagen Paul Pfeifer <hagen@jauu.net>
2011-08-30 22:19:41 +0200

    start to bring in the old functionality
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- captcp.py
+++ captcp.py
@@ -7,15 +7,99 @@ import sys
 import os
 import logging
 import optparse
+import dpkt
+import pcap
+import socket
+import struct
+import inspect
+
+# required debian packages:
+#   python-dpkt
+#   python-pypcap 
 
 __author__  = "Hagen Paul Pfeifer"
 __version__ = "0.5"
 __license__ = "GPLv3"
 
+
+# TCP flag constants
+TH_URG = dpkt.tcp.TH_URG
+TH_ACK = dpkt.tcp.TH_ACK
+TH_PSH = dpkt.tcp.TH_PUSH
+TH_RST = dpkt.tcp.TH_RST
+TH_SYN = dpkt.tcp.TH_SYN
+TH_FIN = dpkt.tcp.TH_FIN
+
+# Protocols
+TCP = dpkt.tcp.TCP
+UDP = dpkt.udp.UDP
+
+
 class ExitCodes:
     EXIT_SUCCESS  = 0
     EXIT_ERROR    = 1
     EXIT_CMD_LINE = 2
+
+class Colors:
+
+    colors = [
+        '\033[91m',
+        '\033[92m',
+        '\033[93m',
+        '\033[94m',
+        '\033[95m'
+    ]
+
+    start = 0
+
+    ENDC    = '\033[0m'
+
+    @staticmethod
+    def next_color():
+        Colors.start += 1
+        return Colors.colors[Colors.start % len(Colors.colors)]
+
+
+class Converter:
+
+    def dotted_quad_num(ip):
+        "convert decimal dotted quad string to long integer"
+        return struct.unpack('I', socket.inet_aton(ip))[0]
+    dotted_quad_num = staticmethod(dotted_quad_num)
+
+
+    def num_to_dotted_quad(n):
+        "convert long int to dotted quad string"
+        return socket.inet_ntoa(struct.pack('I', n))
+    num_to_dotted_quad = staticmethod(num_to_dotted_quad)
+
+    def make_mask(n):
+        "return a mask of n bits as a long integer"
+        return (1L << n)-1
+
+    make_mask = staticmethod(make_mask)
+
+
+    def dpkt_addr_to_string(addr):
+        iaddr = int(struct.unpack('I', addr)[0])
+        return Converter.num_to_dotted_quad(iaddr)
+
+    dpkt_addr_to_string  = staticmethod(dpkt_addr_to_string)
+
+
+    def ip_to_net_host(ip, maskbits):
+        "returns tuple (network, host) dotted-quad addresses given IP and mask size"
+
+        n = Converter.dotted_quad_num(ip)
+        m = Converter.makeMask(maskbits)
+
+        host = n & m
+        net = n - host
+
+        return Converter.num_to_dotted_quad(net), Converter.num_to_dotted_quad(host)
+
+    ip_to_net_host = staticmethod(ip_to_net_host)
+
 
 
 class Highlight:
@@ -108,9 +192,6 @@ class Captcp:
 
         classname = Captcp.modes[submodule.lower()]
         return classname
-
-
-
 
 
     def __init__(self):
