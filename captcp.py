e3cca58277f7229d91caf87b8b674a1e0574947f
Hagen Paul Pfeifer <hagen@jauu.net>
2013-01-28 20:59:03 +0100

    core: add function to copy a directory structure
    Needed for the much larger html file set for the animation module. May
    be needed for other modules as well.
    
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- captcp.py
+++ captcp.py
@@ -20,6 +20,7 @@ import datetime
 import subprocess
 import select
 import re
+import shutil
 
 # optional packages
 try:
@@ -279,6 +280,46 @@ class U:
         if b == 0: return 0.0
         return float(a) / b * 100
 
+
+    @staticmethod
+    def copytree(src, dst, symlinks=False, ignore=None):
+        names = os.listdir(src)
+        if ignore is not None:
+            ignored_names = ignore(src, names)
+        else:
+            ignored_names = set()
+
+        os.makedirs(dst)
+        errors = []
+        for name in names:
+            if name in ignored_names:
+                continue
+            srcname = os.path.join(src, name)
+            dstname = os.path.join(dst, name)
+            try:
+                if symlinks and os.path.islink(srcname):
+                    linkto = os.readlink(srcname)
+                    os.symlink(linkto, dstname)
+                elif os.path.isdir(srcname):
+                    copytree(srcname, dstname, symlinks, ignore)
+                else:
+                    copy2(srcname, dstname)
+                # XXX What about devices, sockets etc.?
+            except (IOError, os.error) as why:
+                errors.append((srcname, dstname, str(why)))
+            # catch the Error from the recursive copytree so that we can
+            # continue with other files
+            except Error as err:
+                errors.extend(err.args[0])
+        try:
+            copystat(src, dst)
+        except WindowsError:
+            # can't copy file access times on Windows
+            pass
+        except OSError as why:
+            errors.extend((src, dst, str(why)))
+        if errors:
+            raise Error(errors)
 
 
 class RainbowColor:
